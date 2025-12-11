52544a17c691b84f399f5a138780f11f288e6399
Luis <luisazamudio1489@gmail.com>
2025-12-11 09:28:15 -0800

    small format kinematics test

--- code/subsystems/SwerveDriveSubsystem.py
+++ code/subsystems/SwerveDriveSubsystem.py
@@ -107,6 +107,8 @@ class SwerveDriveSubsystemClass(commands2.Subsystem):
         self.kinematics = SwerveDrive2Kinematics(
             Translation2d(+W/2, +L/2),  # Front Left
             Translation2d(+W/2, -L/2),  # Front Right
+            Translation2d(-L/2, W/2), # Back Left 
+            Translation2d(-L/2, -W/2) # Back Right 
         )
 
         # --------------- GYRO ---------------
@@ -131,3 +133,4 @@ class SwerveDriveSubsystemClass(commands2.Subsystem):
         # Apply module speeds and angles
         self.front_left.set(states[0].speed, states[0].angle.radians())
         self.front_right.set(states[1].speed, states[1].angle.radians())
+
